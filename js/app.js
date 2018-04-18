f3fb8c9e4b8f7489de8bbcbeac39b82b638e2be9
didudidudu <36870660+didudidudu@users.noreply.github.com>
2018-04-18 15:40:00 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -30,6 +30,28 @@ deerResume.controller('resumeCtrl', function ($scope,$http,storage) {
   else 
     url = baseurl+"?a=show&domain="+encodeURIComponent(window.location);
 
+  function getInfo(){
+	var viewpass = '1234'; // 密码必须大于3位。留空则任何人可以访问
+	var title = '标题';
+	var subtitle = '子标题';
+	var content = 'MarkDown';
+	
+	var data = {};
+	
+	data.local = 1;
+		
+		data.errno = '0';
+		data.show = 1;
+		data.title = title;
+		data.subtitle = subtitle;
+		data.content = content;
+	
+	
+	return data;
+	//console.log(data);
+}
+
+  
   $scope.resume = getInfo();
   console.log($scope.resume);
 //   $http.get(url).success(function( data ){
