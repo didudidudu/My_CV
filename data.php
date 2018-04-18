c01ab275fa9586e5528a9c9f54fb2de1da7e28f0
didudidudu <36870660+didudidudu@users.noreply.github.com>
2018-04-18 15:01:20 +0800

    Update data.php

--- data.php
+++ data.php
@@ -1,5 +1,3 @@
-<?php
-
 $viewpass = '1234'; // 密码必须大于3位。留空则任何人可以访问
 $title = '标题';
 $subtitle = '子标题';
@@ -26,5 +24,6 @@ else
 	$data['content'] = $content;
 
 }
-
+<?php
 echo json_encode( $data );
+?>
