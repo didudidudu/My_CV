d145984e6c0f619c21da4f49e6ae5e6626d91cc0
didudidudu <36870660+didudidudu@users.noreply.github.com>
2018-04-18 15:23:24 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,20 @@
+function getInfo(){
+	var viewpass = '1234'; // 密码必须大于3位。留空则任何人可以访问
+	var title = '标题';
+	var subtitle = '子标题';
+	var content = 'MarkDown';
+	
+	var data = {};
+	
+	data.local = 1;
+		
+		data.errno = '0';
+		data.show = 1;
+		data.title = title;
+		data.subtitle = subtitle;
+		data.content = content;
+	
+	
+	return data;
+	//console.log(data);
+}
